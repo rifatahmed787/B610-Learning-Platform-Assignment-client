c80a9eeb7526e383b36709b2e96f48b0ab4da5d8
Rifat <mdrifatahmed787@gmail.com>
2022-10-25 00:57:08 +0600

    header added from kitwind

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8uMjJ5ULR9r7K_x_7pi7WxPr0OKZPDZQ",
+  authDomain: "react-assignment-three.firebaseapp.com",
+  projectId: "react-assignment-three",
+  storageBucket: "react-assignment-three.appspot.com",
+  messagingSenderId: "24433502069",
+  appId: "1:24433502069:web:52d5c995e187f1b861553c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
