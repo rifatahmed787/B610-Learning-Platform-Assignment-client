c62bf0540b46d96c45729dddc55ef2f5131002f0
Rifat <mdrifatahmed787@gmail.com>
2022-10-25 02:49:44 +0600

    blog first question added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,58 @@ import React from "react";
 const Blog = () => {
   return (
     <div>
-      <p>hello blog</p>
+      <section className="dark:bg-gray-800 dark:text-gray-100">
+        <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+          <h2 className="text-2xl font-semibold sm:text-4xl">
+            Frequently Asked Questions
+          </h2>
+          <p className="mt-4 mb-8 dark:text-gray-400">
+            Some Questions and Answers are given below that you want to know.
+          </p>
+          <div className="space-y-4">
+            <details className="w-full border rounded-lg">
+              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+                1.1 what is cors?
+              </summary>
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                Cors stands for Cross-Origin Resource Sharing (CORS).It is a
+                browser security feature that restricts cross-origin HTTP
+                requests that are initiated from scripts running in the browser.
+              </p>
+            </details>
+            <details className="w-full border rounded-lg">
+              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+                1.2 Why are you using firebase? What other options do you have
+                to implement authentication?
+              </summary>
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna
+                porttitor egestas tincidunt neque vehicula potenti.{" "}
+              </p>
+            </details>
+            <details className="w-full border rounded-lg">
+              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+                1.3 How does the private route work?
+              </summary>
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                Justo libero tellus integer tincidunt justo semper consequat
+                venenatis aliquet imperdiet. Ultricies urna proin fusce nulla
+                pretium sodales vel magna et massa euismod vulputate sed.{" "}
+              </p>
+            </details>
+            <details className="w-full border rounded-lg">
+              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+                1.4 What is Node? How does Node work?
+              </summary>
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+                Justo libero tellus integer tincidunt justo semper consequat
+                venenatis aliquet imperdiet. Ultricies urna proin fusce nulla
+                pretium sodales vel magna et massa euismod vulputate sed.{" "}
+              </p>
+            </details>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
